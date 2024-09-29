7a422fcddfdc03a42ca2a22a2d8a16bba5323f2b
sarahatesyou <sarafcso@gmail.com>
2024-09-29 17:40:27 +0100

    fixed a bug

--- src/java.js
+++ src/java.js
@@ -12,16 +12,8 @@ function generateRecipe(event) {
 
   let mainIngredient = document.querySelector("#main-ingredient");
   let apiKey = "o091fdfe309a88f508fe60bcaa4tc41a";
-  let context = `You are a vegetarian cuisine expert and love to create easy vegetarian recipes. Your mission is to generate an easy and fast vegetarian recipe in basic HTML, with the following format:<h2>the recipe title</h2>
-    <h3>ingredients:</h3>
-          <ul>
-            <li>first ingredient</li>
-            <li>so on</li>
-            <li>and so on</li>
-          </ul>
-
-          <h3>directions:</h3>
-          the recipe goes here. Make sure to follow the user instructions.`;
+  let context =
+    "You are a vegetarian cuisine expert and love to create easy vegetarian recipes. Your mission is to generate an easy and fast vegetarian recipe, with the following format: the recipe title inside an <h2>, write ingredients inside an <h3>, then make the list of ingredients with <li> but don't add a marker, write directions inside an <h3>directions:</h3>, and finally write the step-by-step instructions, each step separated with <br />. Make sure to follow the user instructions.";
   let prompt = `User instructions: Generate a vegetarian recipe where the main ingredient is ${mainIngredient.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
